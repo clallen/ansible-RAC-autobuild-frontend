accd95be085179dca85e4b561a3c1121eb997fc7
Clint Allen <clint.allen@ni.com>
2016-09-16 16:49:31 -0500

    Renamed chassis to domain
    Added type/use field to storage
    
    Added form-handling for domain page

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import Form
-from wtforms import StringField, SubmitField, SelectField
+from wtforms import StringField, SubmitField, SelectField, RadioField
 from wtforms.validators import DataRequired
 
 class ClusterNameForm(Form):
@@ -13,18 +13,20 @@ class StorageForm(Form):
     end = StringField("blockend", validators=[DataRequired()])
     size = StringField("blocksize", validators=[DataRequired()])
     chassis = StringField("blockchassis", validators=[DataRequired()])
+    use = RadioField("blockuse", choices=[ ("os", "OS"), ("env", "DB Env") ])
     addbtn = SubmitField(label="Add Storage Block")
     backbtn = SubmitField(label="<< Back")
     nextbtn = SubmitField(label="Next >>")
 
-class ChassisForm(Form):
+class DomainForm(Form):
     name = StringField("domname", validators=[DataRequired()])
     cores = StringField("domcores", validators=[DataRequired()])
     ram = StringField("domram", validators=[DataRequired()])
     pvid = StringField("dompvid", validators=[DataRequired()])
     pclass = SelectField("dompclass", choices=[ ("dev", "Dev"), ("test", "Test"), ("prod", "Prod") ])
-    pg = SelectField("dompg", choices=[ ("pre_release", "Pre-release"), ("release", "Release"), ("post_release", "Post-release") ])
+    pgroup = SelectField("dompgroup", choices=[ ("pre_release", "Pre-release"), ("release", "Release"), ("post_release", "Post-release") ])
     racdisks = StringField("domracdisks", validators=[DataRequired()])
     chassis = StringField("domchassis", validators=[DataRequired()])
+    addbtn = SubmitField(label="Add Domain")
     backbtn = SubmitField(label="<< Back")
     nextbtn = SubmitField(label="Next >>")
--- app/views.py
+++ app/views.py
@@ -17,6 +17,7 @@ def clname():
         if re.search(r"/storage$", str(request.referrer)) is None:
             session.clear()
             session["blocks"] = {}
+            session["domains"] = {}
         else:
             form.clname.data = session["clname"]
     return render_template("clname.html", title="Start", form=form)
@@ -32,7 +33,8 @@ def storage():
                                                       "begin": form.begin.data,
                                                       "end": form.end.data,
                                                       "size": form.size.data,
-                                                      "chassis": form.chassis.data }
+                                                      "chassis": form.chassis.data,
+                                                      "use": form.use.data }
             else:
                 flash("All fields are required")
         elif "rmbtn" in request.form:
@@ -43,7 +45,7 @@ def storage():
             if len(session["blocks"]) == 0:
                 flash("Storage must be defined before continuing")
             else:
-                return redirect("/chassis")
+                return redirect("/domain")
     form.name.data = ""
     form.pool.data = ""
     form.ports.data = ""
@@ -51,9 +53,36 @@ def storage():
     form.end.data = ""
     form.size.data = ""
     form.chassis.data = ""
+    form.use.data = ""
     return render_template("storage.html", title="Storage", form=form)
 
-@app.route("/chassis", methods=["GET", "POST"])
-def chassis():
-    form = ChassisForm()
-    return render_template("chassis.html", title="Chassis", form=form)
+@app.route("/domain", methods=["GET", "POST"])
+def domain():
+    form = DomainForm()
+    if request.method == "POST":
+        if form.addbtn.data:
+            if form.validate():
+                session["domains"][form.name.data] = { "cores": form.cores.data,
+                                                       "ram": form.ram.data,
+                                                       "pvid": form.pvid.data,
+                                                       "pclass": form.pclass.data,
+                                                       "pgroup": form.pgroup.data,
+                                                       "racdisks": form.racdisks.data,
+                                                       "chassis": form.chassis.data }
+            else:
+                flash("All fields are required")
+        elif "rmbtn" in request.form:
+            session["domains"].pop(request.form["rmbtn"].split()[1])
+        elif form.backbtn.data:
+            return redirect("/storage")
+        elif form.nextbtn.data:
+            pass
+    form.name.data = ""
+    form.cores.data = ""
+    form.ram.data = ""
+    form.pvid.data = ""
+    form.pclass.data = ""
+    form.pgroup.data = ""
+    form.racdisks.data = ""
+    form.chassis.data = ""
+    return render_template("domain.html", title="Domain", form=form)
